7ccc4f591a07961811ef75db2ba19124d2808d9f
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-08-26 13:30:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-//your JS code here. If required.
 const form = document.getElementById('myForm');
 const btn = document.getElementById('btn');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault(); // Prevent default form submission
-
+  
   const name = document.getElementById('name').value;
   const age = parseInt(document.getElementById('age').value);
-
-  if (name === '' || age === '') {
+  
+  if (name === '' || isNaN(age)) {
     alert('Please fill in all fields.');
     return;
   }
-
+  
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
       if (age >= 18) {
@@ -22,11 +21,12 @@ form.addEventListener('submit', (event) => {
       }
     }, 4000);
   });
-
+  
   promise
     .then((name) => {
       alert(`Welcome, ${name}. You can vote.`);
     })
     .catch((name) => {
-  alert(`Oh sorry, ${name}. You aren't old enough.`); // No need to escape here
+      alert(`Oh sorry ${name}. You aren't old enough.`);
+    });
 });
